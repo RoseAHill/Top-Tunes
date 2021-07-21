3fdd89f38ee2f82a38f7b8102f0abcc1e9d97a35
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-07-21 15:00:19 -0400

    remove empty reference

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.use(methodOverride('_method'))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(
   express.static(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
